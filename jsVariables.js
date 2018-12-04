dec373bc0daf9a88b4eea75083a6b2a21d3c7d11
Keolasin <43076895+keolasin@users.noreply.github.com>
2018-12-03 17:47:17 -0800

    JavaScript variables checkpoint code

---
+++ jsVariables.js
@@ -0,0 +1,13 @@
+/*
+Using let, var, and const, create 3 variables that hold your name, state, and hair color.
+*/
+
+// declaring three variables
+const myName = 'Matthew';
+var myState = 'California';
+let myHairColor = 'Brown';
+
+// checking values of those three variables
+console.log(myName);
+console.log(myState);
+console.log(myHairColor);
