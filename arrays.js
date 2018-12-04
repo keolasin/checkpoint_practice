3879621d33067b7fc5fb45f5712ece32cbdb54b6
Keolasin <43076895+keolasin@users.noreply.github.com>
2018-12-03 20:48:11 -0800

    Arrays checkpoint

---
+++ arrays.js
@@ -0,0 +1,11 @@
+/*
+Using the clothes variable below use the push() method to add a new item to the clothes array.
+The new item should be called pants.
+*/
+var clothes = ['collared shirt', 'shirt'];
+
+// add code below
+clothes.push('pants');
+// add code above
+
+console.log(clothes);
