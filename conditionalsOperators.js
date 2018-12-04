b7c0f6a386e9805cb8bff7159cf5f2286ca14d22
Keolasin <43076895+keolasin@users.noreply.github.com>
2018-12-03 20:26:14 -0800

    Conditionals & Operators checkpoint

---
+++ conditionalsOperators.js
@@ -0,0 +1,9 @@
+/*
+Create a variable called grade and set it to the grade you plan to receive on this assignment. Then write an if statement that will check whether the grade is equal to an A, B, C, D, or F and inform the user if they passed or failed using console.log().
+*/
+var grade = 'A';
+if (grade == 'A'){
+  console.log("You've passed!");
+} else {
+  console.log("You failed :(");
+};
