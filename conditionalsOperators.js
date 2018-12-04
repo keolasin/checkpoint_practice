f14dcecdcd99d568390b3edde7d7786e74239fe8
Keolasin <43076895+keolasin@users.noreply.github.com>
2018-12-03 23:29:36 -0800

    Revised conditionals exercise

--- conditionalsOperators.js
+++ conditionalsOperators.js
@@ -3,7 +3,15 @@ Create a variable called grade and set it to the grade you plan to receive on th
 */
 var grade = 'A';
 if (grade == 'A'){
+  console.log("You've passed exceptionally!");
+} else if (grade == 'B'){
+  console.log("You've done really well and passed!");
+} else if (grade == 'C'){
   console.log("You've passed!");
+} else if (grade == 'D'){
+  console.log("This isn't very good, but you've barely passed.");
+} else if (grade == 'F' || grade == 'E'){
+  console.log("You've failed :(");
 } else {
-  console.log("You failed :(");
+  console.log("I'm not sure what that is, but you didn't pass...");
 };
